8de9d312653ad767e6a3edf878327c8ccbe47435
Маис <Kozannostra777@mail.ru>
2022-06-21 21:51:42 +0300

    Черновик_1/

---
+++ Черновик_1/111index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <script lang="JavaScript">
+        let name = promt("Введите имя");
+        alert("Привет, " + name)
+        let age = promt("Введите возрост");
+        let anyKey = confirm("Вы уверены");
+        alert(anyKey);
+        if (anyKey == true) {
+            alert("kjekvj");
+        } else {
+            alert("kjekvj");
+        }
+
+        switch (age) {
+            case 18:  // if (x === 'value1')
+                alert("Теперь можно ВСЕ))");
+                break;
+            case 10:  
+                alert("Быстро домой!");
+                break;
+            case 30:  
+                alert("Иди женись)");
+                break;                
+            
+
+            default:
+            alert("sdofjvnodn")
+              
+                
+        }
+    </script>
+</body>
+
+</html>
+
+
+
---
+++ Черновик_1/array.php
@@ -0,0 +1,10 @@
+<?php
+return[
+    'Hobbis' => [
+        'hob_1' => ['Adobe Photoshop', '15%',],
+        'hob_1' => ['Photography', '80%'],
+        'hob_1' => ['Illustrator','75%'],
+        'hob_1' => ['Media', '40%']
+    ]
+];
+?>
